2df619e0f12725efd9e77c7148fac8bd7fa39eaa
Zaven <petrosyan_zaven@inbox.ru>
2019-02-10 20:25:47 +0400

    client refactor

--- client/src/components/ClientComponent.vue
+++ client/src/components/ClientComponent.vue
@@ -1,22 +1,9 @@
 <template>
-  <div class="hello">
-    <div id="todo-list-example" class="container">
-      <div class="row">
-        <div class="col-md-6 mx-auto">
-          <h1 class="text-center">Clients</h1>
-          <form v-on:submit.prevent="addClient">
-            <label for="clientnameinput">Client name</label>
-              <input type="text" v-model="clientname" id="clientnameinput" class="form-control" placeholder="Client name">
-            <label for="clientphoneinput">Client phone</label>
-              <input type="text" v-model="phone" id="clientphoneinput" class="form-control" placeholder="Client phone">
-            <label for="clientemailinput">Client email</label>
-              <input type="text" v-model="email" id="clientemailinput" class="form-control" placeholder="Client">
-
-            <button v-if="this.isEdit==false" type="submit" class="btn btn-success btn-block mt-3">Submit</button>
-            <button v-else v-on:click="updateClient()" class="btn btn-success btn-block mt-3">Update</button>
-          </form>
-          <table class="table">
-            <tr v-for="(client) in clients" 
+  <div id="app">
+    <h1>Clients</h1>
+    <div id="clients-list" class="container">
+       <table class="table">
+            <tr v-for="client in clients" 
             v-bind:key="client.id" 
             v-bind:name="client.name" 
             v-bind:email="client.email"
@@ -27,15 +14,43 @@
               <td class="text-left">{{ client.phone }}</td>
               <td class="text-left">{{ client.providers }}</td>
               <td class="text-right">
-                <button class="btn btn-info" v-on:click="editClient(client.name, client._id, client.phone,client.email, client.providers)">Edit</button>
+                <button class="btn btn-info" v-on:click="editClient(client.name, client._id, client.phone, client.email, client.providers)">Edit</button>
                 <button class="btn btn-danger" v-on:click="deleteClient(client._id)">Delete</button>
               </td>
             </tr>
           </table>
+    </div>
+    <div class="add-client">
+    <div class="row">
+    <form v-on:submit.prevent="addClient" class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input v-model="name" id="nameinput" type="text" placeholder="Client name" class="validate">
+          <label  class="active" for="nameinput">First Name</label>
         </div>
       </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input v-model="email" id="clientemailinput" type="email" class="validate" placeholder="email">
+          <label class="active" for="clientemailinput">Email</label>
+        </div>
+         <div class="input-field col s12">
+          <input v-model="phone" id="clientphoneinput" type="text" class="validate"  placeholder="Client phone">
+          <label class="active" for="clientphoneinput">Phone</label>
+        </div>
+      </div>
+      <select class="select-provider">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </select>
+    </form>
     </div>
-  </div>
+  <button v-if="this.isEdit==false" type="submit">Submit</button>
+   <button v-else v-on:click="updateClient()">Update</button>
+    </div>
+</div>
 </template>
 
 <script>
@@ -46,55 +61,95 @@ export default {
   data() {
     return {
       clients: [],
+      providers: [],
       id: '',
-      clientname: '',
+      name: '',
       phone: '',
       email: '',
-      providers: [],
       isEdit: false
     }
   },
-
-  mounted() {
+  mounted () {
     this.getClients();
+    this.getProviders();
   },
-  methods: {
+   methods: {
+    getProviders() {
+      axios.get('http://localhost:5001/providers').then(response => {
+        this.providers = response.data.data
+      })
+    },
     getClients() {
-      axios.get('http://localhost:5001/clients').then(result => {
-        console.log(result.data);
-        this.clients = result.data;
-      }).catch(err => console.error(err));
+       axios.get('http://localhost:5001/clients').then(response => {
+      this.clients = response.data.data
+    });
     },
     addClient() {
-      axios.post('http://localhost:5001/clients', { name: this.clientname,phone: this.phone, email: this.email, providers: this.providers })
-      .then((res) => {
-        this.clientname = ''
+      axios.post('http://localhost:5001/clients', {
+        name: this.name,
+        phone: this.phone,
+        email: this.email,
+        providers: this.providers
+      }).then(() => {
+        this.name = '',
         this.getClients()
-        console.log(res)
-      }).catch(err => console.error(err));
+      });
     },
-    editClient(name, id, phone, email, providers) {
+    editClient(id, name, email, phone, providers) {
       this.id = id,
-      this.clientname = name,
-      this.phone = phone,
-      this.email = email,
+      this.name = name,
+      this.phone =phone,
+      this.email =email,
       this.providers = providers
     },
     updateClient() {
-      axios.put('http://localhost:5001/${this.id}', {name: this.clientname, phone: this.phone, email: this.email, providers: this.providers})
-      .then((res) => {
-        this.clientname = '',
-        this.isEdit = false,
-        this.getClients()
-      }).catch(err => console.error(res));
+      axios.put('http://localhost://5001/${this.id}', {
+        name: this.name,
+        phone: this.phone,
+        email: this.email,
+        providers: this.providers
+      }).then(() => {
+        this.name = '',
+        this.getClients(),
+        this.isEdit = false
+      });
     },
     deleteClient(id) {
-      axios.delete('http://localhost:5001/clients/$id').then((res) => {
-        this.clientname = '',
+      axios.delete('http://localhost:5001/${id}').then(() => {
+        this.name = '',
         this.getClients()
-        console.log(res)
-      }).catch(err => console.error(err));
+      });
     }
   }
 }
 </script>
+
+<style>
+.row {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
